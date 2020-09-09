5d751a3cbe6e22b8a0a5b03b5e52d1e6d11ffc9d
Marcos Yañez <marcos080399@hotmail.com>
2020-09-09 01:05:55 -0500

    Biografia Kemonito

--- lib/Biofighter.dart
+++ lib/Biofighter.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Biofighter extends StatelessWidget {
   String urlKemonito =
       "https://imagenes.milenio.com/bphSgOtpXqNAf5Zn_9i1QK3v2fs=/958x596/smart/https://www.milenio.com/uploads/media/2020/03/24/kemonito-santiago-chaparro_0_9_958_596.jpeg";
+  String bioKemonito =
+      'Kemonito nacio el 3 de julio de 1967, es un icono de la lucha libre profesional mexicano, acualmente trabaja para la empresa Consejo Mundial de Lucha Libre (CMLL)';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +24,23 @@ class Biofighter extends StatelessWidget {
           Container(color: Colors.purpleAccent[700]),
           Column(
             children: [
+              Expanded(
+                child: Container(),
+              ),
               luchadorBio(),
+              Expanded(
+                child: Container(),
+              ),
+              Text(
+                'Mas Luchadores',
+                style: TextStyle(
+                  color: Color.fromRGBO(220, 229, 227, 0.9),
+                  fontSize: 29,
+                ),
+              ),
+              Expanded(
+                child: Container(),
+              ),
             ],
           )
         ],
@@ -35,16 +53,77 @@ class Biofighter extends StatelessWidget {
       child: Container(
         height: 385,
         width: 350,
-        decoration: BoxDecoration(
-          color: Colors.indigo[50],
-          borderRadius: BorderRadius.circular(40),
-        ),
         child: Stack(
           children: [
             Image(
               width: 350,
               image: NetworkImage(urlKemonito),
-            )
+            ),
+            Positioned(
+              top: 155,
+              left: 10,
+              child: Container(
+                width: 330,
+                height: 210,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[700],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'Kemonito',
+                      style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.yellow[200],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5),
+                      child: Text(
+                        bioKemonito,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.yellow[50],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FlatButton(
+                          color: Colors.teal[400],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          onPressed: () {},
+                          child: Text(
+                            'Ver mas',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        FlatButton(
+                          color: Colors.pink[400],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Text(
+                                'Me gusta',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Icon(Icons.favorite_border),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
